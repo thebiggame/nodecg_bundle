3cb941210157be354a614024d5c1a22766deb96a
duck <me@duck.me.uk>
2018-10-27 11:57:33 +0100

    Typo in alert.js, whoops

--- extension/alert.js
+++ extension/alert.js
@@ -2,7 +2,7 @@
 'use strict';
 
 module.exports = function (nodecg) {
-    const alertData = nodecg.Replicant('alert:data', {defaultValue: 'Pay Attention!', persistent: true);
+    const alertData = nodecg.Replicant('alert:data', {defaultValue: 'Pay Attention!', persistent: true});
     const alertFlair = nodecg.Replicant('alert:flair', {defaultValue: false, persistent: false});
     const alertDelay = nodecg.Replicant('alert:delay', {defaultValue: false, persistent: false});
 };
