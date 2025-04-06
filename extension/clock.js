c7950d4284edea592f59cd17420a97a9a5a4729f
duck. <me@duck.me.uk>
2025-04-06 22:03:12 +0100

    Remove compiled files (these should be compiled individually)

--- extension/clock.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const nodecg_1 = require("./util/nodecg");
-const replicants_1 = require("./util/replicants");
-(0, nodecg_1.get)().log.trace('Extension clock loaded.');
-function tick() {
-    replicants_1.repClock.value = new Date().toISOString();
-}
-const timer = setInterval(tick, 1000);
