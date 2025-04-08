e3f4a6a80aa4e065bce3ea7f156da6793f9ba9dd
duck. <me@duck.me.uk>
2025-04-08 16:57:13 +0100

    Add guide widget to dashboard

---
+++ src/dashboard/guide/guide.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <p><b>Welcome to NG-TBG!</b></p>
+      <p>
+        For help using NodeCG, BIGbot, or anything else, please reach out to
+        <i>duck.</i>
+      </p>
+    </div>
+  </div>
+</template>
