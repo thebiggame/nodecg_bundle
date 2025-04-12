1e340481339e1963960f23c14c55fd857b219ee5
duck. <me@duck.me.uk>
2025-04-12 20:04:14 +0100

    New stage-prep graphic
    This is a large clock intended to be used during setup to assist with team timekeeping.

---
+++ src/graphics/stage-prep/clock/clock.vue
@@ -0,0 +1,328 @@
+<script setup lang="ts">
+import { ExampleType } from '@thebiggame/types'
+import {
+  Configschema,
+  ProjectorActive,
+  Clock,
+  BandwidthData,
+  MusicData,
+} from '@thebiggame/types/schemas'
+import { useReplicant, useAssetReplicant, Asset } from 'nodecg-vue-composable'
+import { onMounted, watch, toValue, useTemplateRef, computed } from 'vue'
+import { gsap, Quart } from 'gsap'
+
+import {
+  RiDownloadCloudFill,
+  RiUploadCloudFill,
+  RiCalendarEventFill,
+  RiHandHeartFill,
+  RiEmotionHappyFill,
+  RiRocketFill,
+  RiMusic2Fill,
+  RiUserLine,
+  RiWifiFill,
+  RiLockPasswordLine,
+  RiShieldUserFill,
+  RiTimeLine,
+  RiBugLine,
+  RiGameFill,
+  RiGamepadLine,
+} from '@remixicon/vue'
+
+// Access the bundle configuration with types.
+const config = nodecg.bundleConfig as Configschema
+
+const versionString = 'Bundle-' + nodecg.bundleVersion
+
+// Accessing normal types.
+// const exampleType: ExampleType = { exampleProperty: 'exampleString' };
+
+const tl = gsap.timeline({ autoRemoveChildren: true })
+
+const repMusicData = useReplicant<MusicData>('music:data', 'thebiggame')
+
+const clockRep = useReplicant<Clock>('clock', 'thebiggame')
+
+const clockTime = computed(() => {
+  const period = new Date(clockRep?.data as Clock)
+  return period.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1')
+})
+
+const clockEventDay = computed(() => {
+  const period = new Date(clockRep?.data as Clock)
+  switch (period.getDay()) {
+    case 5:
+      return 1
+    case 6:
+      return 2
+    case 0:
+      return 3
+    default:
+      return '?'
+  }
+})
+</script>
+
+<style scoped>
+:host {
+  position: absolute;
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+  /* #3c438c */
+  color: #fff;
+}
+
+.wipe-outer {
+  position: absolute;
+  overflow: hidden;
+  white-space: nowrap;
+  top: 10px;
+  left: 10px;
+  right: 10px;
+  bottom: 500px;
+  background-color: #fff;
+  border-top: solid #fff 5px;
+  border-bottom: solid #fff 5px;
+  border-left: solid #fff 20px;
+  border-right: solid #fff 5px;
+  border-radius: 0px 500px 500px 0px;
+}
+
+.wipe-inner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  font-size: 35px;
+  color: white;
+  background-color: #3c438c;
+  overflow: hidden;
+}
+
+#wipe-title {
+  font-size: 45px;
+  font-weight: 300;
+  font-family: 'Archivo Variable', 'Arial Black', 'Arial Bold', sans-serif;
+  text-shadow: rgba(0, 0, 0, 0.5) 0 0 20px;
+  background-color: #5762d7;
+  border-right: solid #fff 2px;
+}
+
+.box-elem {
+  height: 75px;
+  border-left: solid #fff 2px;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+
+.box-elem-message {
+  height: 75px;
+  border-left: solid #fff 2px;
+  padding-left: 0px;
+  padding-right: 10px;
+  font-size: 50px;
+}
+
+.box-border-left {
+  border-left: solid #303030 1px;
+}
+
+.event-num {
+  color: goldenrod;
+  font-family: 'Archivo Variable', sans-serif;
+  font-weight: 800;
+}
+
+.icon-primary {
+  --iron-icon-height: 30px;
+  --iron-icon-width: 30px;
+  padding-right: 5px;
+}
+
+.icon-lead {
+  --iron-icon-height: 100px;
+  --iron-icon-width: 100px;
+  padding-right: 15px;
+}
+
+.info-body {
+  font-size: 45px;
+  line-height: 125%;
+  text-wrap: wrap;
+  /* Chromism */
+  white-space: normal;
+}
+
+.box-clock {
+  font-family: 'Arial Black', 'Arial Bold', Gadget, sans-serif;
+  font-weight: 500;
+  font-size: 45px;
+  display: inline;
+}
+
+.day-trail {
+  font-size: 25px;
+  color: #909090;
+  padding-right: 5px;
+}
+
+.is-message {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  padding-left: 5px;
+}
+
+.icon-message {
+  margin-top: -10px;
+  margin-bottom: -10px;
+}
+
+.anim-wiggle {
+  animation: wobble 1s infinite;
+  display: inline-block;
+}
+
+.is-message p {
+  display: inline-block;
+}
+
+#day-box {
+  font-size: 40px;
+}
+
+#now-body {
+  font-size: 75px;
+  font-weight: 700;
+}
+
+#schedule {
+  padding-top: 10px;
+  padding-left: 10px;
+}
+
+/*
+.icon-bw {
+  --iron-icon-height: 30px;
+          --iron-icon-width: 30px;
+}
+.bw-trail {
+  font-size: 15px;
+  color: #909090;
+  padding-left: 2px;
+  vertical-align:bottom;
+}
+*/
+
+.icon-bw {
+  font-size: 25px;
+}
+
+.bw-trail {
+  font-size: 15px;
+  color: #909090;
+  padding-left: 2px;
+  vertical-align: bottom;
+}
+
+.day-trail {
+  font-size: 25px;
+  color: #909090;
+  padding-left: 2px;
+  vertical-align: bottom;
+}
+
+.bw-content {
+  font-size: 25px;
+  font-family: 'DSEG14 Classic', 'Archivo Variable', sans-serif;
+  font-weight: 300;
+}
+
+.bw-high-anim {
+  color: rgb(255, 127, 127);
+  animation: tilt-shaking 0.15s infinite;
+  transition: color 2s ease-in-out;
+}
+
+@keyframes tilt-shaking {
+  0% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+  25% {
+    transform: translate(1px, 1px) rotate(5deg);
+  }
+  50% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+  75% {
+    transform: translate(-1px, 1px) rotate(-5deg);
+  }
+  100% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+}
+
+@keyframes wobble {
+  0% {
+    transform: rotate(-5deg);
+  }
+  50% {
+    transform: rotate(5deg);
+  }
+  100% {
+    transform: rotate(-5deg);
+  }
+}
+
+.event-trail {
+  font-size: 30px;
+  font-weight: 300;
+  padding-right: 10px;
+  font-family: 'Arial Bold', Gadget, sans-serif;
+}
+
+#next-body {
+  font-size: 50px;
+  font-weight: 500;
+}
+
+#wipe-next {
+  margin-top: -20px;
+}
+
+.sponsor-boxo > .img-fluid {
+  max-height: 40px;
+}
+
+#music-box > .img-fluid {
+  max-height: 20px;
+}
+</style>
+
+<template>
+  <div>
+    <!--DON'T REMOVE THIS DIV OTHERWISE ANIMATION BREAKS JUST TRUST ME OKAY-->
+    <div ref="wipe-outer" class="wipe-outer">
+      <div ref="wipe-inner" class="wipe-inner container-fluid">
+        <div id="body" class="row">
+          <div
+            id="clock-box"
+            class="d-flex flex-fill align-items-center justify-content-center align-items-center"
+          >
+            <div
+              class="box-clock align-self-center pl-1 pr-3"
+              style="font-size: 350px"
+              id="clock"
+            >
+              {{ clockTime }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/graphics/stage-prep/main.vue
@@ -0,0 +1,157 @@
+<script setup lang="ts">
+import { ExampleType } from '@thebiggame/types'
+import { Configschema } from '@thebiggame/types/schemas'
+import { useHead } from '@vueuse/head'
+import { ref } from 'vue'
+
+import Clock from './clock/clock.vue'
+import DatabarPrep from './databar-prep/databar-prep.vue'
+
+// Set the title of this page.
+useHead({ title: 'NodeCG Stage PREP' })
+</script>
+<style scoped>
+body {
+  margin: 0;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.app-container {
+  position: absolute;
+  width: 1920px;
+  height: 1080px;
+  overflow: hidden;
+  font-family: 'Archivo Variable';
+  /* #3c438c */
+  color: #fff;
+  /*background: url("http://i.imgur.com/EcIltyp.png");
+  background: url("http://i.imgur.com/ivbkXt1.png");*/
+}
+
+#loop {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 1920px;
+  height: 1080px;
+}
+
+#timer-outer {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background-color: #fff;
+  padding-right: 5px;
+}
+
+#timer-inner {
+  position: relative;
+  font-size: 32px;
+  color: white;
+  background-color: #3c438c;
+  padding-left: 50px;
+  padding-right: 50px;
+}
+
+#cdown-name {
+  font-size: 25px;
+  font-weight: 300;
+}
+
+#cdown-name:after {
+  content: '';
+  height: 1px;
+  background: linear-gradient(
+    to right,
+    rgba(147, 147, 147, 1) 0%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  display: block;
+}
+
+#cdown-time {
+  font-size: 100px;
+  font-weight: 700;
+  margin-top: -10px;
+}
+
+#clock {
+  font-weight: 500;
+  font-size: 40px;
+  display: inline;
+}
+
+#clock-period {
+  font-size: 25px;
+  color: #909090;
+  display: inline;
+  vertical-align: text-bottom;
+}
+
+#content {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 150px;
+  height: 100%;
+  -webkit-filter: drop-shadow(0 0 8px rgba(0, 0, 0, 0.5));
+}
+
+#darkener {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+  opacity: 0;
+  z-index: 5000;
+}
+
+#logo {
+  height: 280px;
+}
+
+#flavor {
+  font-size: 42px;
+  margin-top: 12px;
+}
+
+#time {
+  font-size: 192px;
+  -webkit-font-feature-settings: 'tnum';
+  font-weight: 600;
+  transition: opacity 333ms ease;
+  opacity: 0;
+}
+
+tbg-alert {
+  z-index: 9001;
+}
+
+#cdown-time {
+  font-family: Consolas, monospace;
+}
+
+#current_data {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background-color: #fff;
+  padding-right: 5px;
+  padding-left: 5px;
+}
+
+#music > .img-fluid {
+  max-height: 40px;
+}
+</style>
+<template>
+  <div class="app-container">
+    <Clock />
+    <DatabarPrep />
+  </div>
+</template>
